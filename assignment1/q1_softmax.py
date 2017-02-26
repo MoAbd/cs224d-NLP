6564d929514aa7523c42fe2a80e302dbd9da9ee9
Mohamed Abdallah <mo.ab.elkasaby@gmail.com>
2017-02-26 17:36:35 +0200

    Solve softmax assignment

--- assignment1/q1_softmax.py
+++ assignment1/q1_softmax.py
@@ -21,10 +21,19 @@ def softmax(x):
     """
 
     ### YOUR CODE HERE
-    raise NotImplementedError
+    if len(x.shape) == 1:
+
+        x = np.exp(x)
+        m = x / np.sum(x)
+    else:
+
+	    N = x.shape[0]
+	    x -= np.max(x, axis=1).reshape(N, 1)
+	    x = np.exp(x)
+	    m = x / np.sum(x, axis=1).reshape(N, 1)
     ### END YOUR CODE
     
-    return x
+    return m
 
 def test_softmax_basic():
     """
@@ -39,13 +48,11 @@ def test_softmax_basic():
 
     test2 = softmax(np.array([[1001,1002],[3,4]]))
     print test2
-    assert np.amax(np.fabs(test2 - np.array(
-        [[0.26894142, 0.73105858], [0.26894142, 0.73105858]]))) <= 1e-6
+    assert np.amax(np.fabs(test2 - np.array([[0.26894142, 0.73105858], [0.26894142, 0.73105858]]))) <= 1e-6
 
     test3 = softmax(np.array([[-1001,-1002]]))
     print test3
-    assert np.amax(np.fabs(test3 - np.array(
-        [0.73105858, 0.26894142]))) <= 1e-6
+    assert np.amax(np.fabs(test3 - np.array([0.73105858, 0.26894142]))) <= 1e-6
 
     print "You should verify these results!\n"
 
@@ -58,7 +65,7 @@ def test_softmax():
     """
     print "Running your tests..."
     ### YOUR CODE HERE
-    raise NotImplementedError
+    
     ### END YOUR CODE  
 
 if __name__ == "__main__":
